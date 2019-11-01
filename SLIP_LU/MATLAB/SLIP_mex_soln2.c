310242ba7393a4579f3a23845dbcd9258b7e68af
clouren <46497332+clouren@users.noreply.github.com>
2019-11-01 13:00:18 -0500

    Add files via upload

--- SLIP_LU/MATLAB/SLIP_mex_soln2.c
+++ SLIP_LU/MATLAB/SLIP_mex_soln2.c
@@ -16,7 +16,9 @@ void mexFunction
     //--------------------------------------------------------------------------
     // Initialize SLIP LU library environment
     //--------------------------------------------------------------------------
-    mp_set_memory_functions(mxMalloc, slip_gmp_mex_realloc, slip_gmp_mex_free);
+    SLIP_initialize_expert(mxMalloc, slip_gmp_mex_realloc, slip_gmp_mex_free);
+
+    //mp_set_memory_functions(mxMalloc, slip_gmp_mex_realloc, slip_gmp_mex_free);
     //SLIP_initialize();
     SLIP_info status;
 
