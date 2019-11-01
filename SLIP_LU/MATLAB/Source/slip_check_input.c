bce655ff89e7d8242f604447e406f704c2c254e0
clouren <46497332+clouren@users.noreply.github.com>
2019-11-01 14:02:34 -0500

    Update and rename SLIP_check_input.c to slip_check_input.c

--- SLIP_LU/MATLAB/Source/slip_check_input.c
+++ SLIP_LU/MATLAB/Source/slip_check_input.c
@@ -1,8 +1,18 @@
+//------------------------------------------------------------------------------
+// SLIP_LU/MATLAB/slip_check_input: Check the input obtained from MATLAB user
+//------------------------------------------------------------------------------
+
+// SLIP_LU: (c) 2019, Chris Lourenco, Jinhao Chen, Erick Moreno-Centeno,
+// Timothy A. Davis, Texas A&M University.  All Rights Reserved.  See
+// SLIP_LU/License for the license.
+
+//------------------------------------------------------------------------------
+
 #include "SLIP_LU_mex.h"
 
 /* Purpose: This function checks the input of the matlab array. It ensures that
    the input matrix and right hand side vectors are correct format. */
-void SLIP_check_input
+void slip_check_input
 (
     const mxArray * input [],     // The matlab input array
     int32_t nargin
