385b812a97992542ac811a5b009f8d8816f19818
clouren <46497332+clouren@users.noreply.github.com>
2019-11-01 14:33:35 -0500

    Update slIp_gmp_mex_free.c

--- SLIP_LU/MATLAB/Source/slIp_gmp_mex_free.c
+++ SLIP_LU/MATLAB/Source/slIp_gmp_mex_free.c
@@ -1,3 +1,13 @@
+//------------------------------------------------------------------------------
+// SLIP_LU/MATLAB/slip_gmp_mex_free: A gmp free function for Matlab mex files
+//------------------------------------------------------------------------------
+
+// SLIP_LU: (c) 2019, Chris Lourenco, Jinhao Chen, Erick Moreno-Centeno,
+// Timothy A. Davis, Texas A&M University.  All Rights Reserved.  See
+// SLIP_LU/License for the license.
+
+//------------------------------------------------------------------------------
+
 #include "SLIP_LU_mex.h"
 
 /* Purpose: A GMP free function. This allows GMP to use
