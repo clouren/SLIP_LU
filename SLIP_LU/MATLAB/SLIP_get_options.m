7a25240182e56ee45926b65c8df7ec4c04b48c9f
clouren <46497332+clouren@users.noreply.github.com>
2019-11-01 12:15:25 -0500

    Update SLIP_get_options.m

--- SLIP_LU/MATLAB/SLIP_get_options.m
+++ SLIP_LU/MATLAB/SLIP_get_options.m
@@ -11,27 +11,19 @@ function x = SLIP_get_options()
 %        2: AMD
 %
 % option.pivot: Pivoting scheme used.
-%        0: Smallest pivot: Default and recommended
+%        0: Smallest pivot
 %        1: Diagonal pivoting
 %        2: First nonzero per column chosen as pivot
-%        3: Diagonal pivoting with tolerance for smallest pivot
+%        3: Diagonal pivoting with tolerance for smallest pivot, Default
 %        4: Diagonal pivoting with tolerance for largest pivot
 %        5: Largest pivot
 % 
-% option.print_level: level of details to be printed
-%        0: print nothing
-%        1: just errors and warnings: Default
-%        2: terse, with basic stats from COLAMD/AMD and SLIP and solution
-%
-% option.check: Set equal to 1 if you want solution checked 
 % option.int: Set equal to 1 if input mat is already integral
 % option.intb: set equal to 1 if input RHS vector(s) are already integral
 % option.tol: tolerance (0,1) which is used if some threshold pivoting is used
 
 x.column = 1;
 x.pivot = 0;
-x.print_level = 0;
-x.check = 0;
 x.int = 0;
 x.intb = 0;
 x.tol = 0.1;
