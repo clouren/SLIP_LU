cefaaac66cda6912a312479eb235b4380a5201fd
clouren <46497332+clouren@users.noreply.github.com>
2019-11-01 13:08:12 -0500

    Update SLIP_LU.m

--- SLIP_LU/MATLAB/SLIP_LU.m
+++ SLIP_LU/MATLAB/SLIP_LU.m
@@ -1,3 +1,13 @@
+//------------------------------------------------------------------------------
+// SLIP_LU/MATLAB/SLIP_LU.m: Interface to SLIP LU within MATLAB
+//------------------------------------------------------------------------------
+
+// SLIP_LU: (c) 2019, Chris Lourenco, Jinhao Chen, Erick Moreno-Centeno,
+// Timothy A. Davis, Texas A&M University.  All Rights Reserved.  See
+// SLIP_LU/License for the license.
+
+//------------------------------------------------------------------------------
+
 function varargout = SLIP_LU(A,b,option)
 % Purpose: Solve exactly the sparse linear system Ax = b where A and b are stored as
 % doubles. A must be stored as a sparse matrix. b must be stored as a dense
