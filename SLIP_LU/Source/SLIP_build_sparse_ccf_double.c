1bb5292a986c4b4d2950fca891dbfc201fbd3a5c
clouren <46497332+clouren@users.noreply.github.com>
2019-11-01 15:38:29 -0500

    Create SLIP_build_sparse_ccf_double.c

---
+++ SLIP_LU/Source/SLIP_build_sparse_ccf_double.c
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------------
+// SLIP_LU/SLIP_build_sparse_ccf_double: build sparse matrix from double
+//------------------------------------------------------------------------------
+
+// SLIP_LU: (c) 2019, Chris Lourenco, Jinhao Chen, Erick Moreno-Centeno,
+// Timothy A. Davis, Texas A&M University.  All Rights Reserved.  See
+// SLIP_LU/License for the license.
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// SLIP_build_sparse_ccf_double: build sparse matrix from double values
+//------------------------------------------------------------------------------
+
+#undef  SLIP_FREE_WORKSPACE
+#define SLIP_FREE_WORKSPACE                 \
+    SLIP_delete_mpz_array(&x_new, nz);
+
+SLIP_info SLIP_build_sparse_ccf_double
+(
+    SLIP_sparse *A_output,// It should be initialized but unused yet
+    int32_t *p,           // The set of column pointers
+    int32_t *I,           // set of row indices
+    double *x,            // Set of values as doubles
+    int32_t n,            // dimension of the matrix
+    int32_t nz            // number of nonzeros in A (size of x and I vectors)
+)
+{
+    SLIP_info ok;
+    if (!p || !I || !x || !A_output)
+    {
+        return SLIP_INCORRECT_INPUT;
+    }
+
+    mpz_t* x_new = SLIP_create_mpz_array(nz);
+    if (!x_new) {return SLIP_OUT_OF_MEMORY;}
+
+    SLIP_CHECK(slip_expand_double_array(x_new, x, A_output->scale, nz));
+
+    // Create our matrix
+    SLIP_CHECK(slip_mpz_populate_mat(A_output, I, p, x_new, n, nz));
+
+    // Free memory
+    SLIP_FREE_WORKSPACE;
+    // Success
+    return SLIP_OK;
+}
