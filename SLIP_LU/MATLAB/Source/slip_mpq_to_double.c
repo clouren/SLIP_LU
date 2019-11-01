f94026d354b06e3051d8fa192ac3855a7ac776a5
clouren <46497332+clouren@users.noreply.github.com>
2019-11-01 14:30:36 -0500

    Update and rename SLIP_mpq_to_double.c to slip_mpq_to_double.c

---
+++ SLIP_LU/MATLAB/Source/slip_mpq_to_double.c
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+// SLIP_LU/MATLAB/slip_mpw_to_double: Convert mpq_t to double
+//------------------------------------------------------------------------------
+
+// SLIP_LU: (c) 2019, Chris Lourenco, Jinhao Chen, Erick Moreno-Centeno,
+// Timothy A. Davis, Texas A&M University.  All Rights Reserved.  See
+// SLIP_LU/License for the license.
+
+//------------------------------------------------------------------------------
+
+# include "SLIP_LU_mex.h"
+
+/* Purpose: This function converts mpq array to double
+ * NOTE: This induces roundoff error via the final division
+*/
+void slip_mpq_to_double
+(
+    double* x_doub,       // double array
+    const mpq_t* x_mpq,   // mpq array
+    const int32_t n       // size of b
+)
+{
+    SLIP_info status;
+    for (int32_t i = 0; i < n; i++)    
+    {
+        SLIP_MEX_OK(slip_mpq_get_d(&(x_doub[i]), x_mpq[i]));
+    }
+}
