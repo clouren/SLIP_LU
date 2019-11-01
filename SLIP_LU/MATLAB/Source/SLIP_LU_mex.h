6fd7ffda334d79d754c85a8540b711f7119cf97c
clouren <46497332+clouren@users.noreply.github.com>
2019-11-01 14:00:53 -0500

    Update SLIP_LU_mex.h

--- SLIP_LU/MATLAB/Source/SLIP_LU_mex.h
+++ SLIP_LU/MATLAB/Source/SLIP_LU_mex.h
@@ -1,3 +1,13 @@
+//------------------------------------------------------------------------------
+// SLIP_LU/MATLAB/SLIP_LU_mex.h: include file for MATLAB functions
+//------------------------------------------------------------------------------
+
+// SLIP_LU: (c) 2019, Chris Lourenco, Jinhao Chen, Erick Moreno-Centeno,
+// Timothy A. Davis, Texas A&M University.  All Rights Reserved.  See
+// SLIP_LU/License for the license.
+
+//------------------------------------------------------------------------------
+
 #ifndef SLIP_mex 
 #define SLIP_mex
 
@@ -23,9 +33,8 @@ void* slip_gmp_mex_realloc
 );
 
 /* Purpose: A GMP free function. This allows GMP to use
- * MATLAB's mxFree instead of free */
-
-// A GMP free function
+ * MATLAB's mxFree instead of free 
+ */
 void slip_gmp_mex_free 
 (
     void* x,    // void* to be freed
@@ -35,37 +44,54 @@ void slip_gmp_mex_free
 /* Purpose: This function converts mpq array to double
  * NOTE: This induces roundoff error via the final division
 */
-void SLIP_mpq_to_double
+void slip_mpq_to_double
 (
     double* x_doub,       // double array
     const mpq_t* x_mpq,   // mpq array
     const int32_t n       // size of b
 );
 
-void SLIP_check_input
+void slip_check_input
 (
     const mxArray * input [],    // The matlab input array
     int32_t nargin
 );
 
-void SLIP_get_matlab_options
+void slip_get_matlab_options
 (
     SLIP_options* option,  // Control parameters
     const mxArray* input   // The input options from MATLAB interface
 );
 
-void SLIP_int32_to_mwIndex(mwIndex* y, int32_t* x, int32_t n) ;
+/* Purpose: Convert int32_t* array to MATLAB mwIndex* array
+ */
+void slip_int32_to_mwIndex
+(
+    mwIndex* y, 
+    int32_t* x, 
+    int32_t n
+) ;
 
-void SLIP_mwIndex_to_int32(int32_t* y, mwIndex* x, mwSize n) ;
+/* Purpose: convert mwIndex* array to int32_t* array
+ */
+void slip_mwIndex_to_int32
+(
+    int32_t* y, 
+    mwIndex* x, 
+    mwSize n
+) ;
 
-void SLIP_mex_check_for_inf
+/* Purpose: Check the input x array for numbers too large for 
+ * double precision.
+ */
+void slip_mex_check_for_inf
 (
     double* x, // The array of numeric values 
     mwSize n   // size of array
 );
 
 /* Purpose: This function reads in the A matrix and right hand side vectors. */
-void SLIP_mex_get_A_and_b
+void slip_mex_get_A_and_b
 (
     SLIP_sparse *A,          // Internal SLIP Mat stored in ccf 
     SLIP_dense *b,           // mpz matrix used internally
@@ -74,19 +100,42 @@ void SLIP_mex_get_A_and_b
 );
 
 
-mxArray* SLIP_mex_output_soln(double** x, int32_t m, int32_t n) ;
+/* Purpose: Output the solution to the linear system Ax=b to matlab
+ */
+mxArray* slip_mex_output_soln
+(
+    double** x, 
+    int32_t m, 
+    int32_t n
+) ;
 
-mxArray* SLIP_mex_output_col_permut(int32_t* x, int32_t n );
+/* Purpose: Output the column permutation the Q matrix 
+ */
+mxArray* slip_mex_output_col_permut
+(
+    int32_t* x, 
+    int32_t n 
+);
 
-mxArray* SLIP_mex_output_p(int32_t* pinv, int32_t n) ;
+/* Purpose: Output the row permutation the P matrix 
+ */
+mxArray* slip_mex_output_p
+(
+    int32_t* pinv, 
+    int32_t n
+) ;
 
-mxArray* SLIP_mex_output_L
+/* Purpose: Output the L matrix 
+ */
+mxArray* slip_mex_output_L
 (
     SLIP_sparse *L,    // the sparse matrix to be output
     mpz_t *rhos        // sequence of pivots
 );
 
-mxArray* SLIP_mex_output_U
+/* Purpose: Output the U matrix 
+ */
+mxArray* slip_mex_output_U
 (
     SLIP_sparse *U,    // the sparse matrix to be output
     mpz_t *rhos,       // sequence of pivots
@@ -94,16 +143,33 @@ mxArray* SLIP_mex_output_U
 
 );
 
-void SLIP_mex_error(SLIP_info status) ;
+/* Purpose: Report errors if they arise
+ */
+void slip_mex_error
+(
+    SLIP_info status
+) ;
 
-/* This function is modified from CSparse/Source/cs_dropzeros*/
-mwIndex SLIP_dropzeros (mxArray *A);
+/* Purpose: Drop entries which are zero from a sparse matrix
+ */
+mwIndex slip_dropzeros 
+(
+    mxArray *A
+);
 
-/* drop entries for which fkeep(A(i,j)) is false; return nz if OK, else -1 */
-mwIndex SLIP_fkeep (mxArray *A, bool (*fkeep) (int32_t, int32_t, double));
+/* Purpose: Used to drop zeros 
+ */
+mwIndex slip_fkeep 
+(
+    mxArray *A, 
+    bool (*fkeep) (int32_t, int32_t, double)
+);
 
-/* A = A' */
-/* This function is modified from CSparse/Source/cs_transpose*/
-SLIP_info SLIP_transpose (mxArray *A);
+/* Purpose: transpose the matrix A
+ */
+SLIP_info slip_transpose 
+(
+    mxArray *A
+);
 
 #endif
