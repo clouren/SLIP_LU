305e7aebc7433445198f447de0ca5e84fd583944
Chris Lourenco <46497332+clouren@users.noreply.github.com>
2020-01-06 16:38:58 -0600

    Add files via upload

--- SLIP_LU/Include/SLIP_LU.h
+++ SLIP_LU/Include/SLIP_LU.h
@@ -119,6 +119,22 @@
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
+//-------------------------External header files--------------------------------
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <gmp.h>
+#include <mpfr.h>
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 //-------------------------Default Parameters-----------------------------------
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
@@ -134,25 +150,6 @@
 
 // Authors of code
 #define SLIP_AUTHOR "Christopher Lourenco, Jinhao Chen, Erick Moreno-Centeno, Timothy Davis"
-
-//------------------------------------------------------------------------------
-// Type of MPFR rounding used. 
-//------------------------------------------------------------------------------
-
-// The MPFR library utilizes an internal rounding scheme. The options are
-//  MPFR_RNDN: round to nearest (roundTiesToEven in IEEE 754-2008),
-//  MPFR_RNDZ: round toward zero (roundTowardZero in IEEE 754-2008),
-//  MPFR_RNDU: round toward plus infinity (roundTowardPositive in IEEE 754-2008),
-//  MPFR_RNDD: round toward minus infinity (roundTowardNegative in IEEE 754-2008),
-//  MPFR_RNDA: round away from zero.
-//  MPFR_RNDF: faithful rounding. This is not stable
-
-// SLIP LU utilizes MPFR_RNDN. If the user wishes to change this, they can change 
-// the following parameter
-
-// TODO Make this an option
-#define SLIP_MPFR_ROUND MPFR_RNDN
-
 
 //------------------------------------------------------------------------------
 // Error codes
@@ -216,14 +213,15 @@ SLIP_col_order ;
 
 typedef struct SLIP_options
 {
-    SLIP_pivot pivot;   // Type of pivoting scheme used.
-    SLIP_col_order order;// Type of column ordering scheme used
-    double tol;         // User specified tolerance for SLIP_TOL_SMALLEST and
-                        // SLIP_TOL_LARGEST
-    int32_t print_level;// 0: print nothing, 1: just errors,
-                        // 2: terse, with basic stats from COLAMD/AMD and SLIP
-                        // 3: all, with matrices and results
-    uint64_t prec;      // Precision used to output file if MPFR is chosen
+    SLIP_pivot pivot;           // Type of pivoting scheme used.
+    SLIP_col_order order;       // Type of column ordering scheme used
+    double tol;                 // User specified tolerance for SLIP_TOL_SMALLEST and
+                                // SLIP_TOL_LARGEST
+    int32_t print_level;        // 0: print nothing, 1: just errors,
+                                // 2: terse, with basic stats from COLAMD/AMD and SLIP
+                                // 3: all, with matrices and results
+    uint64_t prec;              // Precision used to output file if MPFR is chosen
+    mpfr_rnd_t SLIP_MPFR_ROUND; // Type of MPFR rounding used
 } SLIP_options;
 
 /* Purpose: Create and return SLIP_options pointer with default parameters
@@ -425,7 +423,8 @@ SLIP_info SLIP_build_sparse_ccf_double
     int32_t *I,           // set of row indices
     double *x,            // Set of values as doubles
     int32_t n,            // dimension of the matrix
-    int32_t nz            // number of nonzeros in A (size of x and I vectors)
+    int32_t nz,            // number of nonzeros in A (size of x and I vectors)
+    SLIP_options* option
 );
 
 /* Purpose: Build a SLIP_sparse from int stored ccf matrix */
@@ -496,7 +495,8 @@ SLIP_info SLIP_build_sparse_trip_double
     int32_t *J,         // set of column indices
     double *x,          // Set of values in double
     int32_t n,          // dimension of the matrix
-    int32_t nz          // number of nonzeros in A (size of x, I, and J vectors)
+    int32_t nz,          // number of nonzeros in A (size of x, I, and J vectors)
+    SLIP_options* option
 );
 
 /* Purpose: Build a sparse matrix from triplet form where input is int */
@@ -561,7 +561,8 @@ SLIP_info SLIP_build_dense_double
     SLIP_dense *A_output, // Dense matrix, allocated but unused
     double **b,           // Set of values as doubles
     int32_t m,            // number of rows
-    int32_t n             // number of columns
+    int32_t n,             // number of columns
+    SLIP_options* option
 );
 
 /* Purpose: Build a dense matrix from int input */
@@ -863,14 +864,6 @@ SLIP_info SLIP_LU_factorize
     SLIP_options *option    // command options
 );
 
-// TODO: Do we need this function? I don't see the function file though.
-SLIP_info SLIP_determinant
-(
-    SLIP_sparse *A,         // matrix that was factorized by SLIP_LU_factorize
-    mpz_t *rhos,            // sequence of pivots from SLIP_LU_factorize
-    mpq_t determinant       // determinant of A
-) ;
-
 // Solves Ax=b, returning the solution x as a double matrix
 SLIP_info SLIP_solve_double
 (
@@ -934,11 +927,10 @@ SLIP_info SLIP_LU_solve     //solves the linear system LD^(-1)U x = b
 );
 
 // check and print a SLIP_sparse matrix
-// TODO: use SLIP_options as input? YES.
 SLIP_info SLIP_spok  // returns a SLIP_LU status code
 (
     SLIP_sparse *A,     // matrix to check
-    int32_t print_level // 0: print nothing, 1: just errors, 2: terse, 3: all
+    SLIP_options* option  // Determine print level
 ) ;
 
 /* Purpose: Convert the output mpq_t** solution vector obtained from
@@ -963,7 +955,8 @@ SLIP_info SLIP_get_mpfr_soln
     mpfr_t **x_mpfr,      // mpfr solution of size n*numRHS to Ax = b
     mpq_t  **x_mpq,       // mpq solution of size n*numRHS to Ax = b.
     int32_t n,            // Dimension of A, number of rows of x
-    int32_t numRHS        // Number of right hand side vectors
+    int32_t numRHS,        // Number of right hand side vectors
+    SLIP_options* option
 );
 
 /*
@@ -976,5 +969,261 @@ SLIP_info SLIP_check_solution
     mpq_t **x,                // solution vector
     SLIP_dense *b             // right hand side
 );
+
+//------------------------------------------------------------------------------
+//---------------------------SLIP GMP/MPFR Functions----------------------------
+//------------------------------------------------------------------------------
+/* The following functions are the SLIP LU interface to the GMP/MPFR libary. Each 
+ * corresponding GMP/MPFR function is given a wrapper to ensure that no memory 
+ * leaks or crashes occur. All covered GMP functions can be found in SLIP_gmp.c
+ *
+ */
+
+// The GMP library does not handle out-of-memory failures.  However, it does
+// provide a mechanism for passing function pointers that replace GMP's use of
+// malloc, realloc, and free.  This mechanism is used to provide a try/catch
+// mechanism for memory allocation errors, using setjmp and longjmp.
+
+// When a GMP function is called, this wrapper keeps track of a list of objects
+// allocated by that function.  The list is started fresh each time a GMP
+// function is called.  If any allocation fails, the NULL pointer is not
+// returned to GMP.  Instead, all allocated blocks in the list are freed,
+// and slip_gmp_allocate returns directly to wrapper.
+
+
+#ifndef SLIP_GMP_LIST_INIT
+// A size of 32 ensures that the list never needs to be increased in size.
+// The test coverage suite in SLIP_LU/Tcov reduces this initial size to
+// exercise the code, in SLIP_LU/Tcov/Makefile.
+#define SLIP_GMP_LIST_INIT 32
+#endif
+
+// uncomment this to print memory debugging info
+// #define SLIP_GMP_MEMORY_DEBUG
+
+#ifdef SLIP_GMP_MEMORY_DEBUG
+void slip_gmp_dump ( void ) ;
+#endif
+
+
+#define SLIP_GMP_WRAPPER_START                                          \
+{                                                                       \
+    slip_gmp_nmalloc = 0 ;                                              \
+    /* setjmp returns 0 if called from here, or > 0 if from longjmp */  \
+    int32_t slip_gmp_status = setjmp (slip_gmp_environment) ;           \
+    if (slip_gmp_status != 0)                                           \
+    {                                                                   \
+        /* failure from longjmp */                                      \
+        slip_gmp_failure (slip_gmp_status) ;                            \
+        return (SLIP_OUT_OF_MEMORY) ;                                   \
+    }                                                                   \
+}
+#define SLIP_GMPZ_WRAPPER_START(x)                                      \
+{                                                                       \
+    slip_gmpz_archive = (mpz_t *) x;                                    \
+    slip_gmpq_archive = NULL;                                           \
+    slip_gmpfr_archive = NULL;                                          \
+    SLIP_GMP_WRAPPER_START;                                             \
+}
+
+#define SLIP_GMPQ_WRAPPER_START(x)                                      \
+{                                                                       \
+    slip_gmpz_archive = NULL;                                           \
+    slip_gmpq_archive =(mpq_t *) x;                                     \
+    slip_gmpfr_archive = NULL;                                          \
+    SLIP_GMP_WRAPPER_START;                                             \
+}
+
+#define SLIP_GMPFR_WRAPPER_START(x)                                     \
+{                                                                       \
+    slip_gmpz_archive = NULL;                                           \
+    slip_gmpq_archive = NULL;                                           \
+    slip_gmpfr_archive = (mpfr_t *) x;                                  \
+    SLIP_GMP_WRAPPER_START;                                             \
+}
+
+#define SLIP_GMP_WRAPPER_FINISH                                         \
+{                                                                       \
+    /* clear (but do not free) the list.  The caller must ensure */     \
+    /* the result is eventually freed. */                               \
+    slip_gmpz_archive = NULL ;                                          \
+    slip_gmpq_archive = NULL ;                                          \
+    slip_gmpfr_archive = NULL ;                                         \
+    slip_gmp_nmalloc = 0 ;                                              \
+}
+
+// free a block of memory, and also remove it from the archive if it's there
+#define SLIP_SAFE_FREE(p)                                               \
+{                                                                       \
+    if (slip_gmpz_archive != NULL)                                      \
+    {                                                                   \
+        if (p == MPZ_PTR(*slip_gmpz_archive))                           \
+        {                                                               \
+            MPZ_PTR(*slip_gmpz_archive) = NULL ;                        \
+        }                                                               \
+    }                                                                   \
+    else if (slip_gmpq_archive != NULL)                                 \
+    {                                                                   \
+        if (p == MPZ_PTR(MPQ_NUM(*slip_gmpq_archive)))                  \
+        {                                                               \
+            MPZ_PTR(MPQ_NUM(*slip_gmpq_archive)) = NULL ;               \
+        }                                                               \
+        if (p == MPZ_PTR(MPQ_DEN(*slip_gmpq_archive)))                  \
+        {                                                               \
+            MPZ_PTR(MPQ_DEN(*slip_gmpq_archive)) = NULL ;               \
+        }                                                               \
+    }                                                                   \
+    else if (slip_gmpfr_archive != NULL)                                \
+    {                                                                   \
+        if (p == MPFR_REAL_PTR(*slip_gmpfr_archive))                    \
+        {                                                               \
+            MPFR_MANT(*slip_gmpfr_archive) = NULL ;                     \
+        }                                                               \
+    }                                                                   \
+    SLIP_FREE (p) ;                                                     \
+}
+
+extern int64_t slip_gmp_ntrials ;
+
+SLIP_info SLIP_gmp_fprintf(FILE *fp, const char *format, ... );
+
+SLIP_info SLIP_gmp_printf(const char *format, ... );
+
+SLIP_info SLIP_gmp_fscanf(FILE *fp, const char *format, ... );
+
+#if 0
+/* This function is currently unused, but kept here for future reference. */
+SLIP_info slip_mpfr_asprintf (char **str, const char *template, ... );
+
+SLIP_info slip_mpfr_free_str (char *str);
+#endif
+
+SLIP_info SLIP_mpfr_fprintf(FILE *fp, const char *format, ... );
+
+bool slip_gmp_init (void) ;
+
+void slip_gmp_finalize (void) ;
+ 
+void *slip_gmp_allocate (size_t size) ;
+
+void slip_gmp_free (void *p, size_t size) ;
+
+void *slip_gmp_reallocate (void *p_old, size_t old_size, size_t new_size );
+
+void slip_gmp_failure (int32_t status) ;
+
+SLIP_info SLIP_mpz_init(mpz_t x) ;
+
+SLIP_info SLIP_mpz_init2(mpz_t x, const uint64_t size) ;
+
+SLIP_info SLIP_mpz_set(mpz_t x, const mpz_t y);
+
+SLIP_info SLIP_mpz_set_ui(mpz_t x, const uint64_t y) ;
+
+SLIP_info SLIP_mpz_set_si(mpz_t x, const int32_t y);
+
+#if 0
+/* This function is currently unused, but kept here for future reference. */
+SLIP_info SLIP_mpz_set_d(mpz_t x, const double y);
+
+SLIP_info SLIP_mpz_get_d(double *x, const mpz_t y) ;
+#endif
+
+SLIP_info SLIP_mpz_set_q(mpz_t x, const mpq_t y) ;
+
+SLIP_info SLIP_mpz_mul(mpz_t a, const mpz_t b, const mpz_t c) ;
+
+#if 0
+/* This function is currently unused, but kept here for future reference. */
+SLIP_info SLIP_mpz_add(mpz_t a, const mpz_t b, const mpz_t c);
+
+SLIP_info SLIP_mpz_addmul(mpz_t x, const mpz_t y, const mpz_t z) ;
+#endif
+
+SLIP_info SLIP_mpz_submul(mpz_t x, const mpz_t y, const mpz_t z);
+
+SLIP_info SLIP_mpz_divexact(mpz_t x, const mpz_t y, const mpz_t z) ;
+
+SLIP_info SLIP_mpz_gcd(mpz_t x, const mpz_t y, const mpz_t z) ;
+
+SLIP_info SLIP_mpz_lcm(mpz_t lcm, const mpz_t x, const mpz_t y) ;
+
+SLIP_info SLIP_mpz_abs(mpz_t x, const mpz_t y) ;
+
+SLIP_info SLIP_mpz_cmp(int32_t *r, const mpz_t x, const mpz_t y) ;
+
+SLIP_info SLIP_mpz_cmpabs(int32_t *r, const mpz_t x, const mpz_t y) ;
+
+SLIP_info SLIP_mpz_cmp_ui(int32_t *r, const mpz_t x, const uint64_t y) ;
+
+SLIP_info SLIP_mpz_sgn(int32_t *sgn, const mpz_t x) ;
+
+SLIP_info SLIP_mpz_sizeinbase(uint64_t *size, const mpz_t x, int32_t base) ;
+
+SLIP_info SLIP_mpq_init(mpq_t x) ;
+
+SLIP_info SLIP_mpq_set(mpq_t x, const mpq_t y);
+
+SLIP_info SLIP_mpq_set_z(mpq_t x, const mpz_t y);
+
+SLIP_info SLIP_mpq_set_d(mpq_t x, const double y) ;
+
+SLIP_info SLIP_mpq_set_ui(mpq_t x, const uint64_t y, const uint64_t z);
+
+SLIP_info SLIP_mpq_set_num(mpq_t x, const mpz_t y) ;
+
+SLIP_info SLIP_mpq_set_den(mpq_t x, const mpz_t y);
+
+SLIP_info SLIP_mpq_get_den(mpz_t x, const mpq_t y) ;
+
+SLIP_info SLIP_mpq_get_d(double *x, const mpq_t y) ;
+
+SLIP_info SLIP_mpq_abs(mpq_t x, const mpq_t y) ;
+
+SLIP_info SLIP_mpq_add(mpq_t x, const mpq_t y, const mpq_t z) ;
+
+SLIP_info SLIP_mpq_mul(mpq_t x, const mpq_t y, const mpq_t z) ;
+
+SLIP_info SLIP_mpq_div(mpq_t x, const mpq_t y, const mpq_t z) ;
+
+SLIP_info SLIP_mpq_cmp(int32_t *r, const mpq_t x, const mpq_t y) ;
+
+SLIP_info SLIP_mpq_cmp_ui(int32_t *r, const mpq_t x,
+                    const uint64_t num, const uint64_t den) ;
+
+SLIP_info SLIP_mpq_equal(int32_t *r, const mpq_t x, const mpq_t y) ;
+
+SLIP_info SLIP_mpfr_init2(mpfr_t x, const uint64_t size ) ;
+
+#if 0
+/* This function is currently unused, but kept here for future reference. */
+SLIP_info SLIP_mpfr_set(mpfr_t x, const mpfr_t y, const mpfr_rnd_t rnd) ;
+#endif
+
+SLIP_info SLIP_mpfr_set_d(mpfr_t x, const double y, const mpfr_rnd_t rnd) ;
+
+SLIP_info SLIP_mpfr_set_q(mpfr_t x, const mpq_t y, const mpfr_rnd_t rnd ) ;
+
+SLIP_info SLIP_mpfr_set_z(mpfr_t x, const mpz_t y, const mpfr_rnd_t rnd ) ;
+
+SLIP_info SLIP_mpfr_get_z(mpz_t x, const mpfr_t y, const mpfr_rnd_t rnd) ;
+
+SLIP_info SLIP_mpfr_get_d(double *x, const mpfr_t y, const mpfr_rnd_t rnd) ;
+
+SLIP_info SLIP_mpfr_mul(mpfr_t x, const mpfr_t y, const mpfr_t z,
+                    const mpfr_rnd_t rnd) ;
+
+SLIP_info SLIP_mpfr_mul_d(mpfr_t x, const mpfr_t y, const double z,
+                    const mpfr_rnd_t rnd) ;
+
+SLIP_info SLIP_mpfr_div_d(mpfr_t x, const mpfr_t y, const double z,
+                    const mpfr_rnd_t rnd) ;
+
+SLIP_info SLIP_mpfr_ui_pow_ui(mpfr_t x, const uint64_t y, const uint64_t z,
+                    const mpfr_rnd_t rnd) ;
+
+SLIP_info SLIP_mpfr_log2(mpfr_t x, const mpfr_t y, const mpfr_rnd_t rnd );
+
+SLIP_info SLIP_mpfr_free_cache(void);
 
 #endif
