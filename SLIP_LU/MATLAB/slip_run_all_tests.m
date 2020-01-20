b34eb274340a3befb89c182c881216603d73a8f7
Chris Lourenco <46497332+clouren@users.noreply.github.com>
2020-01-20 12:33:24 -0600

    No longer keeping [L U P Q] = SLIP_LU(A) in MATLAB Interface

--- SLIP_LU/MATLAB/slip_run_all_tests.m
+++ SLIP_LU/MATLAB/slip_run_all_tests.m
@@ -8,14 +8,4 @@ x = SLIP_LU(A,b);
 x2 = A\b;
 err1 = norm(x-x2)/norm(x);
 
-% Test SLIP_mex_soln2.c
-[L U P Q x] = SLIP_LU(A, b);
-x2 = A\b;
-err2 = norm(x-x2)/(norm(x));
-err2a = normest(L*U-P*A*Q);
-
-% Test SLIP_mex_soln3.c
-[L U P Q] = SLIP_LU(A);
-err3 = normest(L*U-P*A*Q);
-
-maxerr = max ([err1 err2 err2a err3]) ;
+maxerr = err1;
