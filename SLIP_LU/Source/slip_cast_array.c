272cea53dfdf09ac8380bf07be669a0cff780526
Chris Lourenco <46497332+clouren@users.noreply.github.com>
2020-03-31 09:56:59 -0500

    Update slip_cast_array.c

--- SLIP_LU/Source/slip_cast_array.c
+++ SLIP_LU/Source/slip_cast_array.c
@@ -3,7 +3,7 @@
 //------------------------------------------------------------------------------
 
 // SLIP_LU: (c) 2019-2020, Chris Lourenco, Jinhao Chen, Erick Moreno-Centeno,
-// Timothy A. Davis, Teyas A&M University  All Rights Reserved.  See
+// Timothy A. Davis, Texas A&M University  All Rights Reserved.  See
 // SLIP_LU/License for the license.
 
 //------------------------------------------------------------------------------
